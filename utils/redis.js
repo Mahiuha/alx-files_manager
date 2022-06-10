0d7f2d0398861bb51def48ebd7207db029a1c064
Mahiuha <tiasho.mahiuha@gmail.com>
2022-06-10 23:48:57 +0300

    redis utils

---
+++ utils/redis.js
@@ -0,0 +1,30 @@
+const redis = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const asyncGet = promisify(this.client.get).bind(this.client);
+    const val = await asyncGet(key);
+    return val;
+  }
+
+  async set(key, value, duration) {
+    await this.client.set(key, value);
+    await this.client.expire(key, duration);
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
