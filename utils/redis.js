3742c19c8c0f4a088f769feee7e1899a23927f0a
Mahiuha <tiasho.mahiuha@gmail.com>
2022-06-15 19:24:50 +0300

    Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -4,6 +4,12 @@ const { promisify } = require('util');
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
+
+    this.getAsync = promisify(this.client.get).bind(this.client);
+
+    this.client.on('error', (error) => {
+      console.error(`Redis client not connected to the server: ${error}`);
+    });
   }
 
   isAlive() {
@@ -11,20 +17,20 @@ class RedisClient {
   }
 
   async get(key) {
-    const asyncGet = promisify(this.client.get).bind(this.client);
-    const val = await asyncGet(key);
-    return val;
+    const value = await this.getAsync(key);
+    return value;
   }
 
   async set(key, value, duration) {
-    await this.client.set(key, value);
-    await this.client.expire(key, duration);
+    this.client.set(key, value);
+    this.client.expire(key, duration);
   }
 
+  // delete function
   async del(key) {
-    await this.client.del(key);
+    this.client.del(key);
   }
 }
 
 const redisClient = new RedisClient();
-module.exports = redisClient;
+export default redisClient;
